48a3f8c504c0c3eee20b95326d3f14d45a29e69e
Martin <martyofmca@gmail.com>
2024-03-06 17:27:26 +0000

    log the id for processed job - 0x03

--- 0x03-queuing_system_in_js/6-job_processor.js
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -1,12 +1,12 @@
 const kue = require('kue');
 
 const queue = kue.createQueue();
-const sendNotification = (phoneNumber, message, done) => {
-  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+const sendNotification = (phoneNumber, message, jobId, done) => {
+  console.log(`Job ${jobId}: Sending notification to ${phoneNumber}, with message: ${message}`);
   done();
 };
 
 queue.process('push_notification_code', (job, done) => {
   const { phoneNumber, message } = job.data;
-  sendNotification(phoneNumber, message, done);
+  sendNotification(phoneNumber, message, job.id, done);
 });
